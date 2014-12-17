42d7a3fdfcc750f67d1efccf8f742b766340e13e
Or Levran <or.levran@gmail.com>
2014-12-17 13:03:28 +0200

    Tests structure. unimplemented.

---
+++ Blackjack/src/tests/AllTestsCard.java
@@ -0,0 +1,48 @@
+package tests;
+
+import static org.junit.Assert.*;
+import main.Blackjack;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Case of JUnit tests for Cards actions
+ * @author Elinor, Or, Idan, Kosta
+ *
+ */
+public class AllTestsCard {
+	
+	/**
+	 * Checks if the face of a card is presented as required
+	 */
+	@Test
+	public void testCardFrontDisplay() {
+		
+		
+	}
+	
+	/**
+	 * Checks if the back of a card is presented as required
+	 */
+	@Test
+	public void testCardBackDisplay() {
+		fail("Not yet implemented");
+	}
+	
+	/**
+	 * Checks if the deck presents a stack of cards
+	 */
+	@Test
+	public void testCardStack() {
+		fail("Not yet implemented");
+	}
+	
+	/**
+	 * Checks if hitting "Hit" adds a card to the player's hand
+	 */
+	@Test
+	public void testHitAction() {
+		fail("Not yet implemented");
+	}
+}
---
+++ Blackjack/src/tests/AllTestsPlayer.java
@@ -0,0 +1,22 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Case of JUnit tests for Player actions
+ * @author Elinor, Or, Idan, Kosta
+ *
+ */
+public class AllTestsPlayer {
+
+	/**
+	 * Checks if hitting "Stand" ends the player's turn and starts the dealer's turn
+	 */
+	@Test
+	public void testStandAction() {
+		fail("Not yet implemented");
+	}
+
+}
